7b512e0aa1d68cdd5ce451e914d305cc3b46c325
Prasad <siddheshsalvi525@gmai.com>
2020-01-23 11:33:34 +0530

    ADDED:CalculationsOFPartTimeWage

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -3,9 +3,9 @@ echo "Welcome to EmployeeWage Computation Program"
 EmployeeCheck=$((RANDOM%2))
 if [ $EmployeeCheck -eq 0 ]
 then 
-echo Employee Is Present
+	echo Employee Is Present
 else
-echo Employee Is Absent
+	echo Employee Is Absent
 fi
 WAGEPERHOUR=20
 FULLWORKDAYHOUR=8
